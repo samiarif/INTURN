02fa3baed7c2f6496b84b6dc3d9e0ad7067d361a
samiarif <dazzsemi@gmail.com>
2026-05-24 17:21:44 +0100

    chore: allow next/image to load Vercel Blob + Clerk avatars

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,21 @@ import type { NextConfig } from 'next';
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.public.blob.vercel-storage.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 export default withNextIntl(nextConfig);
